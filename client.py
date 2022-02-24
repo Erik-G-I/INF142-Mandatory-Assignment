142672844fce08dcc37e46eb0849d1d5698a13e1
Ole.Lysne <ole.lysne@student.uib.no>
2022-02-24 09:55:40 +0100

    lagt til server send

--- client.py
+++ client.py
@@ -2,4 +2,9 @@ from socket import socket, AF_INET, SOCK_STREAM
 
 socket = socket(AF_INET, SOCK_STREAM)
 
-socket.connect("localhost", 5555)
+socket.connect(("localhost", 5555))
+
+test = socket.recv(1024).decode()
+print(test)
+input("")
+
--- server.py
+++ server.py
@@ -4,9 +4,18 @@ socket = socket()
 
 socket.bind(("", 5555))
 socket.listen()
+connectedClients = []
 
-connection, addr = socket.accept()
+while len(connectedClients) < 2:
+    connection, addr = socket.accept()
+    connectedClients.append((connection, addr))
 
 while True:
-    print("Got connection from: ", addr)
+
+    print("Got connection from: ", connectedClients[0])
+    print("Got connection from: ", connectedClients[1])
+    connectedClients[0][0].send("poop".encode())
+    connectedClients[1][0].send("poop".encode())
+
+
     break
