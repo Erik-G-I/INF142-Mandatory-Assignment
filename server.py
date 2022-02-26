2f80dd316bc433a311c1cc293c102df7f1ebb804
Erik.Ingebrigtsen <erik.ingebrigtsen@student.uib.no>
2022-02-26 13:27:59 +0100

    lagt til threading functions fra slides

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
+import imp
 from socket import socket
+from threading import Thread
 
 socket = socket()
 
 socket.bind(("", 5555))
 socket.listen()
 connectedClients = []
+
+# code from slides, Lecture 7
+def accept(socket):
+    while True:
+        connection, addr = socket.accept()
+        print("Accepted", connection, "from", addr)
+        Thread(target=read, args=(connection)).start()
+
+def read(connection):
+    while True:
+        data = connection.recv(1024)
+        if data:
+            sentence = data.decode()
+            new_sentence = sentence.upper()
+            connection.send(new_sentence.encode())
+        else:
+            print('closing', connection)
+            connection.close()
+            break
 
 while len(connectedClients) < 2:
     connection, addr = socket.accept()
