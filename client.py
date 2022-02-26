0e193fbd0de1b456e1c0c39f3aabb42637b80a16
Ole Vik Lysne <oleviklysne@gmail.com>
2022-02-26 17:04:38 +0100

    lagt til playerID og turnbased logikk

--- client.py
+++ client.py
@@ -1,8 +1,24 @@
 from socket import socket, AF_INET, SOCK_STREAM
+from time import sleep
 
 socket = socket(AF_INET, SOCK_STREAM)
-
 socket.connect(("localhost", 5555))
 
-test = socket.recv(1024).decode()
-print(test)
+initialCon = socket.recv(1024).decode()
+print(initialCon)
+
+
+playerID = socket.recv(1).decode()
+print(f"You are Player {playerID}")
+print()
+
+
+playerTurn = socket.recv(1).decode()
+if playerTurn == playerID:
+    print("It is your turn")
+    input("Choose a champion: ")
+else:
+    print("It is your opponent's turn")
+
+
+input()
--- server.py
+++ server.py
@@ -1,43 +1,46 @@
 from socket import socket
 from threading import Thread
+import threading
+from time import sleep
 
 socket = socket()
 
 socket.bind(("", 5555))
 socket.listen()
-connectedClients = []
+threads = []
 
 # code from slides, Lecture 7
 def accept(socket):
     while True:
         connection, addr = socket.accept()
         print("Accepted", connection, "from", addr)
-        Thread(target=read, args=(connection,)).start()
+        thread = Thread(target=read, args=(connection,))
+        threads.append(thread)
+        thread.start()
 
 def read(connection):
     connection.send(str.encode("Connected"))
+    playerID = str(len(threads)).encode()
+    connection.send(playerID)
+    
+    while len(threads) < 2:
+        continue
+
     while True:
-        data = connection.recv(1024)
-        if data:
-            sentence = data.decode()
-            new_sentence = sentence.upper()
-            connection.send(new_sentence.encode())
-        else:
-            print('closing', connection)
-            connection.close()
-            break
+        playerTurn = "1"
+        connection.send(playerTurn.encode())
+
+
+
+        # data = connection.recv(1024)
+        # if data:
+        #     sentence = data.decode()
+        #     new_sentence = sentence.upper()
+        #     connection.send(new_sentence.encode())
+        # else:
+        #     print('closing', connection)
+        #     connection.close()
+        #     break
+
 
 accept(socket)
-# while len(connectedClients) < 2:
-#     connection, addr = socket.accept()
-#     connectedClients.append((connection, addr))
-
-# while True:
-
-#     print("Got connection from: ", connectedClients[0])
-#     print("Got connection from: ", connectedClients[1])
-#     connectedClients[0][0].send("poop".encode())
-#     connectedClients[1][0].send("poop".encode())
-
-
-#     break
